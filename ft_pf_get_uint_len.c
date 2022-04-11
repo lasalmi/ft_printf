a9bfd9e6001533979600487817205bfad713da88
Lauri Salmi <lasalmi@student.hive.fi>
2022-04-11 07:29:36 +0300

    Fixed octal with higher width than prec

--- ft_pf_get_uint_len.c
+++ ft_pf_get_uint_len.c
@@ -6,7 +6,7 @@
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 23:57:24 by lasalmi           #+#    #+#             */
-/*   Updated: 2022/04/10 16:21:07 by lasalmi          ###   ########.fr       */
+/*   Updated: 2022/04/11 06:19:46 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,7 +93,7 @@ void	ft_pf_get_uint_data(t_vardata *vardata, unsigned long long nb, int base, t_
 		else
 			vardata->conv_len += 2;	
 	}
-	if (vardata->conv_len < strdata->width && strdata->precision <= vardata->intlen)
+	if (vardata->conv_len < strdata->width /*&& strdata->precision <= vardata->intlen*/)
 	{
 		vardata->padlen = (strdata->width - vardata->conv_len);
 //		vardata->conv_len += vardata->padlen;
