9f006fb848cac46106ce066daf470330fb1877f2
Lauri Salmi <lasalmi@student.hive.fi>
2022-04-27 14:44:58 +0300

    Added output fail handling

--- ft_pf_print.c
+++ ft_pf_print.c
@@ -6,17 +6,33 @@
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 12:05:12 by lasalmi           #+#    #+#             */
-/*   Updated: 2022/03/22 19:30:05 by lasalmi          ###   ########.fr       */
+/*   Updated: 2022/04/27 14:43:13 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	ft_output_error(t_ft_controller *ft_controller, t_strdata *strdata)
+{
+	ft_controller->chars_written = -1;
+	free(strdata->variable_str);
+	strdata->variable_str = NULL;
+	free(strdata->padding);
+	strdata->padding = NULL;
+	ft_controller->stage = FT_END;
+	return (1);
+}
+
 static void	ft_pf_print_padding(t_ft_controller *ft_controller, t_strdata *strdata)
 {
-	if (!strdata->padding)
+	int	ret;
+
+	ret = 0;
+	if (!strdata->padding || ft_controller->stage == FT_END)
 		return ;
-	write(1, strdata->padding, strdata->padlen);
+	ret = write(1, strdata->padding, strdata->padlen);
+	if (ret < 0 && ft_output_error(ft_controller, strdata))
+		return ;
 	ft_controller->chars_written += strdata->padlen;
 	strdata->padlen = 0;
 	free(strdata->padding);
@@ -25,9 +41,14 @@ static void	ft_pf_print_padding(t_ft_controller *ft_controller, t_strdata *strda
 
 static void	ft_pf_print_variable(t_ft_controller *ft_controller, t_strdata *strdata)
 {
-	if (!strdata->variable_str)
+	int	ret;
+
+	ret = 0;
+	if (!strdata->variable_str || ft_controller->stage == FT_END)
 		return ;
-	write(1, strdata->variable_str, strdata->strlen);
+	ret = write(1, strdata->variable_str, strdata->strlen);
+	if (ret < 0 && ft_output_error(ft_controller, strdata))
+		return ;
 	ft_controller->chars_written += strdata->strlen;
 	strdata->strlen = 0;
 	free(strdata->variable_str);
@@ -43,6 +64,8 @@ void	ft_pf_print(t_ft_controller *ft_controller, t_strdata *strdata)
 		ft_pf_print_variable(ft_controller, strdata);
 	ft_pf_print_padding(ft_controller, strdata);
 	ft_pf_print_variable(ft_controller, strdata);
+	if (ft_controller->stage == FT_END)
+		return ;
 	ft_init_strdata(strdata);
 	ft_controller->stage = FT_READ_PRINT;
 }
